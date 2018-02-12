eccbcd93d21be53931de4560f233cd140fe2ce66
Rulty <Rulty@users.noreply.github.com>
2018-02-12 17:32:04 +0100

    Filter Image in Grey
    -adding a button for the filter
    -filter the image

--- src/BlackAndWhite/ImagePanel.java
+++ src/BlackAndWhite/ImagePanel.java
@@ -1,7 +1,11 @@
 package BlackAndWhite;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,7 +14,7 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ImagePanel extends JPanel {
-	
+
 	final private JFileChooser fileChooser = new JFileChooser();
 	private String path = "";
 	private BufferedImage image;
@@ -18,20 +22,15 @@ public class ImagePanel extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		try{
-			image = ImageIO.read(new File(path));
-			
-		}catch(IOException e) {
-			if(path != "") {
-				jop.showMessageDialog(this, "Error in loading the file", "Error", JOptionPane.ERROR_MESSAGE);
-			}
-		}
+		//open the image
 		
+	    
+	    
 		g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
 	}
 	
 	
-	public void setImage() {
+	public boolean setImage() {
 		
 		//File chooser and opening Image
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG Images", "jpg");
@@ -41,10 +40,30 @@ public class ImagePanel extends JPanel {
 		//Check user have chosen a file
 		if(returnVal == JFileChooser.APPROVE_OPTION) {
 			path = fileChooser.getSelectedFile().getPath();	
+			try{
+				image = ImageIO.read(new File(path));
+			}catch(IOException e) {
+				if(path != "") {
+					JOptionPane.showMessageDialog(this, "Error in loading the file", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 			this.repaint();
+			//reset file chooser for the next time
+			fileChooser.setSelectedFile(null);
+			return true;
+		}else {
+			//reset file chooser for the next time
+			fileChooser.setSelectedFile(null);
+			return false;
 		}
-		
-		//reset file chooser for the next time
-		fileChooser.setSelectedFile(null);
 	}
+	
+	public void setImageBlackAndWhite() {
+		ColorSpace grey = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+		ColorSpace colorSpace = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+		ColorConvertOp ccOp = new ColorConvertOp(colorSpace, grey, null);
+		ccOp.filter(image, image);
+		this.repaint();
+	}
+
 }
--- src/BlackAndWhite/Window.java
+++ src/BlackAndWhite/Window.java
@@ -1,30 +1,39 @@
 package BlackAndWhite;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class Window extends JFrame {
 	
 	private JMenuBar menuBar;
-	private JMenu menu, submenu;
+	private JMenu menu;
 	private JMenuItem menuItem;
-	
 	private ImagePanel imagePanel = new ImagePanel();
-	
+	private JButton button = new JButton("Black And White !");
+	private JPanel container = new JPanel();
+	private JPanel containerButtons = new JPanel();
 	
 	
 	public Window() {
+		
 		this.setSize(1200,800);
 		this.setTitle("Black And White");
 		this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    //imagePanel.setBackground(Color.BLACK);
+	    containerButtons.add(button);
+	    
+	    container.setBackground(Color.BLACK);
+	    container.setLayout(new BorderLayout());
+	    container.add(imagePanel);
+	    container.add(containerButtons, BorderLayout.SOUTH);
+	    button.setVisible(false);
+	    button.addActionListener(new BlackAndWhiteListener());
+	    
 		//construct the menuBar
 		menuBar = new JMenuBar();
 		//First Menu
@@ -51,15 +60,19 @@ public class Window extends JFrame {
 		menuItem.addActionListener(new CloseListener());
 		menu.add(menuItem);
 		
+		//set Menubar, content Pane in the window
 		this.setJMenuBar(menuBar);
-		this.setContentPane(imagePanel);
+		this.setContentPane(container);
 		this.setVisible(true);
 	}
 	
 	//Listener for Open Menu
 	class OpenListener implements ActionListener{
 		public void actionPerformed(ActionEvent arg0) {
-			imagePanel.setImage();	
+			if(imagePanel.setImage()) {
+				button.setVisible(true);
+			}
+			
 		}
 		
 	}
@@ -77,5 +90,11 @@ public class Window extends JFrame {
 			}
 			
 		}
+		//listener for Black and White button
+	class BlackAndWhiteListener implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			imagePanel.setImageBlackAndWhite();
+		}
+	}
 
 }
